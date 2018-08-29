9575aef6009a00e8f185e180b3d6bf8ec4ab55ec
vietson6103@gmail.com <Hn0912388240>
2018-08-29 10:34:33 +0700

    update class CategoryController

--- src/main/java/com/codegym/controller/CategoryController.java
+++ src/main/java/com/codegym/controller/CategoryController.java
@@ -1,13 +1,13 @@
 package com.codegym.controller;
 
+import com.codegym.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
 public class CategoryController {
-    @GetMapping("/")
-    public String showForm(Model model) {
-        return "list";
-    }
+    @Autowired
+    private CategoryService categoryService;
 }
