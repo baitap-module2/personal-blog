27dba327875947e4329692bd109744dec6283a36
vietson6103@gmail.com <Hn0912388240>
2018-08-29 10:39:54 +0700

    update method show category list

--- src/main/java/com/codegym/controller/CategoryController.java
+++ src/main/java/com/codegym/controller/CategoryController.java
@@ -1,13 +1,24 @@
 package com.codegym.controller;
 
+import com.codegym.model.Category;
 import com.codegym.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller("/")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    //    hiển thị danh sách
+    @GetMapping("/categories")
+    public ModelAndView listCategories() {
+        Iterable<Category> categories = categoryService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/category/list");
+        modelAndView.addObject("categories", categories);
+        return modelAndView;
+    }
 }
