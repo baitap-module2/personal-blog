a137eb09693e7cd6fc73fd0d2b3ffda9693b59d9
vietson6103@gmail.com <Hn0912388240>
2018-08-29 23:07:39 +0700

    add method create new category

--- src/main/java/com/codegym/controller/CategoryController.java
+++ src/main/java/com/codegym/controller/CategoryController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -19,6 +21,22 @@ public class CategoryController {
         Iterable<Category> categories = categoryService.findAll();
         ModelAndView modelAndView = new ModelAndView("/category/list");
         modelAndView.addObject("categories", categories);
+        return modelAndView;
+    }
+
+//    thêm
+    @GetMapping("/create-category")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/category/create");
+        modelAndView.addObject("category", new Category());
+        return modelAndView;
+    }
+
+    @PostMapping("/create-category")
+    public ModelAndView saveCategory(@ModelAttribute("category") Category category) {
+        ModelAndView modelAndView = new ModelAndView("/category/create");
+        modelAndView.addObject("category", new Category());
+        modelAndView.addObject("message", "New category created successfully");
         return modelAndView;
     }
 }
