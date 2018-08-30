f53c75cd5fb65c16fa2673797f515c0ac1e4004d
vietson6103@gmail.com <Hn0912388240>
2018-08-30 22:49:47 +0700

    add class BlogServiceImpl

---
+++ src/main/java/com/codegym/service/BlogServiceImpl.java
@@ -0,0 +1,30 @@
+package com.codegym.service;
+
+import com.codegym.model.Blog;
+import com.codegym.repository.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BlogServiceImpl implements BlogService {
+    @Autowired
+    private BlogRepository blogRepository;
+
+    @Override
+    public Iterable<Blog> findAll() {
+        return blogRepository.findAll();
+    }
+
+    @Override
+    public Blog findById(Long id) {
+        return blogRepository.findOne(id);
+    }
+
+    @Override
+    public void save(Blog blog) {
+        blogRepository.save(blog);
+    }
+
+    @Override
+    public void remove(Long id) {
+        blogRepository.delete(id);
+    }
+}
