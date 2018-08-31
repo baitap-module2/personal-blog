e96e12f7cb96a468714a2f67903c415c5e7dcf93
vietson6103@gmail.com <Hn0912388240>
2018-08-31 11:41:05 +0700

    add method show blog list

--- src/main/java/com/codegym/controller/BlogController.java
+++ src/main/java/com/codegym/controller/BlogController.java
@@ -1,11 +1,14 @@
 package com.codegym.controller;
 
+import com.codegym.model.Blog;
 import com.codegym.model.Category;
 import com.codegym.service.BlogService;
 import com.codegym.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BlogController {
@@ -19,5 +22,14 @@ public class BlogController {
     @ModelAttribute("categories")
     Iterable<Category> categories() {
         return categoryService.findAll();
+    }
+
+    //    hiển thị danh sách
+    @GetMapping("/blogs")
+    public ModelAndView listBlog() {
+        Iterable<Blog> blogs = blogService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/blog/list");
+        modelAndView.addObject("blogs", blogs);
+        return modelAndView;
     }
 }
