fe4fb8f428ccf1a5339b48ce2e81ae106df67fe1
vietson6103@gmail.com <Hn0912388240>
2018-08-31 11:33:58 +0700

    add list of categories to all model of view

--- src/main/java/com/codegym/controller/BlogController.java
+++ src/main/java/com/codegym/controller/BlogController.java
@@ -1,9 +1,11 @@
 package com.codegym.controller;
 
+import com.codegym.model.Category;
 import com.codegym.service.BlogService;
 import com.codegym.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class BlogController {
@@ -12,4 +14,10 @@ public class BlogController {
 
     @Autowired
     private BlogService blogService;
+
+    //    gắn danh sách các category vào tất cả các model của view
+    @ModelAttribute("categories")
+    Iterable<Category> categories() {
+        return categoryService.findAll();
+    }
 }
