2dff0fca029a5ef43043415a59610c22f421d4b8
vietson6103@gmail.com <Hn0912388240>
2018-08-31 13:49:15 +0700

    add method edit a blog

--- src/main/java/com/codegym/controller/BlogController.java
+++ src/main/java/com/codegym/controller/BlogController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -48,6 +49,29 @@ public class BlogController {
         ModelAndView modelAndView = new ModelAndView("/blog/create");
         modelAndView.addObject("blog", new Blog());
         modelAndView.addObject("message", "New blog created successfully");
+        return modelAndView;
+    }
+
+    //    sửa
+    @GetMapping("/edit-blog/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Blog blog = blogService.findById(id);
+        if (blog != null) {
+            ModelAndView modelAndView = new ModelAndView("/blog/edit");
+            modelAndView.addObject("blog", blog);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-blog")
+    public ModelAndView updateBlog(@ModelAttribute("blog") Blog blog) {
+        blogService.save(blog);
+        ModelAndView modelAndView = new ModelAndView("/blog/edit");
+        modelAndView.addObject("blog", blog);
+        modelAndView.addObject("message", "Blog updated successfully");
         return modelAndView;
     }
 }
