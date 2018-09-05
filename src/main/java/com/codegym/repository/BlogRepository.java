8c2e02dc647738223e9ca6c7dde68cfac1be0878
vietson6103@gmail.com <Hn0912388240>
2018-09-05 21:21:26 +0700

    repository

--- src/main/java/com/codegym/repository/BlogRepository.java
+++ src/main/java/com/codegym/repository/BlogRepository.java
@@ -1,7 +1,10 @@
 package com.codegym.repository;
 
 import com.codegym.model.Blog;
+import com.codegym.model.Category;
 import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.sql.Blob;
 
 public interface BlogRepository extends PagingAndSortingRepository<Blog, Long> {
 }
