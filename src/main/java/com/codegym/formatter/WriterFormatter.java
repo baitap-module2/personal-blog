b4d171c65ce4ceb5c5164b30f60ab5ad1d70a8f6
vietson6103@gmail.com <Hn0912388240>
2018-09-20 09:44:01 +0700

    fix WriterFormatter

--- src/main/java/com/codegym/formatter/WriterFormatter.java
+++ src/main/java/com/codegym/formatter/WriterFormatter.java
@@ -2,6 +2,7 @@ package com.codegym.formatter;
 
 import com.codegym.model.Writer;
 import com.codegym.service.WriterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,11 @@ import java.util.Locale;
 @Component
 public class WriterFormatter implements Formatter<Writer> {
     private WriterService writerService;
+
+    @Autowired
+    public WriterFormatter(WriterService writerService) {
+        this.writerService = writerService;
+    }
 
     @Override
     public Writer parse(String text, Locale locale) throws ParseException {
